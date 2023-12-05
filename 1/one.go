1915bfa68405d313fc700422a3725360fff13ac0
Michael van Nes <m.f.van.nes@gmail.com>
2023-12-05 22:25:21 +0100

    1 part 2

--- 1/one.go
+++ 1/one.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"regexp"
 	"strconv"
 )
 
@@ -17,14 +16,42 @@ func main() {
 	}
 	scan := bufio.NewScanner(f)
 
+	numberStringsToNumberMap := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+
 	result := 0
 	for scan.Scan() {
 		line := scan.Text()
-		r := regexp.MustCompile("([0-9])]")
 
-		res := r.FindAllString(line, -1)
+		numbers := []string{}
+		for index, token := range line {
+			stringToken := string(token)
+			_, err := strconv.Atoi(stringToken)
+			if nil == err {
+				numbers = append(numbers, stringToken)
+				continue
+			}
 
-		combined := res[0] + res[len(res)-1]
+			for numberAsString, number := range numberStringsToNumberMap {
+				if index+len(numberAsString) > len(line) {
+					continue
+				}
+				if numberAsString == line[index:index+len(numberAsString)] {
+					numbers = append(numbers, number)
+					break
+				}
+			}
+		}
+		combined := numbers[0] + numbers[len(numbers)-1]
 		i, _ := strconv.Atoi(combined)
 		result = result + i
 	}
